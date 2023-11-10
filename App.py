83567551560bb03e277e71fb85399f9f16a6d09d
Ice <150374552+adapttech3d@users.noreply.github.com>
2023-11-10 11:17:16 -0500

    uploading correct version of App.py

--- App.py
+++ App.py
@@ -1,50 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov  8 11:21:01 2023
+Created on Fri Nov 10 10:59:47 2023
 
-@author: icecold
+@author: IceCold
 """
 
-import requests
-import json
+from flask import Flask, request, render_template
+import sqlite3
 
-# Retrieving your Last.fm API key
-with open("config.json") as config_file:
-    config = json.load(config_file)
+app = Flask(__name__)
 
-API_KEY = config["api_key"]
+@app.route('/', methods=['GET', 'POST'])
+def search_lastfm():
+    if request.method == 'POST':
+        query = request.form['query']
 
-def search_lastfm(query):
-    # Define the Last.fm API endpoint for search
-    url = f"http://ws.audioscrobbler.com/2.0/?method=track.search&track={query}&api_key={API_KEY}&format=json"
+        conn = sqlite3.connect('lastfm.db')
+        cursor = conn.cursor()
 
-    try:
-        # Send a GET request to the Last.fm API
-        response = requests.get(url)
+        cursor.execute('''
+            SELECT artist_name, album_title, track_title
+            FROM Jazz
+            WHERE
+                artist_name LIKE ? OR
+                album_title LIKE ? OR
+                track_title LIKE ?
+        ''', (f'%{query}%', f'%{query}%', f'%{query}%'))
 
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the JSON response
-            data = response.json()
-            
-            # Extract and process the search results
-            if 'results' in data and 'trackmatches' in data['results']:
-                tracks = data['results']['trackmatches']['track']
-                for track in tracks:
-                    print("Song Name:", track['name'])
-                    print("Artist:", track['artist'])
-                    print("Album:", track['album'])
-                    print("\n")
-            else:
-                print("No results found.")
+        search_results = cursor.fetchall()
 
+        conn.close()
+
+        if search_results:
+            return render_template('index.html', search_results=search_results)
         else:
-            print("Failed to retrieve data from Last.fm. Status code:", response.status_code)
+            return render_template('index.html', no_matches=True)
 
-    except Exception as e:
-        print("An error occurred:", str(e))
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    search_query = input("Enter a search query: ")
-    search_lastfm(search_query)
+if __name__ == '__main__':
+    app.run(debug=True)
